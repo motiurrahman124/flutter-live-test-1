15030a8c8b55d5de48bc48e08eec1382a389be3e
motiurrahman124 <motiurr503@gmail.com>
2025-04-06 17:13:13 +0600

    setup

---
+++ bin/live-test_1_task.dart
@@ -0,0 +1,25 @@
+abstract class Vehicle{
+  int _speed = 0;
+
+  void move();
+
+  void setSpeed(int speed){
+    _speed = speed;
+  }
+
+  int get speed => _speed;
+}
+
+class Car extends Vehicle{
+
+  @override
+  void move(){
+    print("The car is moving at $_speed km/h");
+  }
+}
+
+void main(){
+ Car newCar = Car();
+ newCar.setSpeed(100);
+ newCar.move();
+}
